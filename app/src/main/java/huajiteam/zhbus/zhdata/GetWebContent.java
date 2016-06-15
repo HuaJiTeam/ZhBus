ed657857245df812361f9b5542d105f3f3a94dac
unknown <>
2016-06-15 23:53:31 +0800

    V2.1.1-2.1.3

--- app/src/main/java/huajiteam/zhbus/zhdata/GetWebContent.java
+++ app/src/main/java/huajiteam/zhbus/zhdata/GetWebContent.java
@@ -9,7 +9,7 @@ import java.io.IOException;
  */
 class GetWebContent {
     private OkHttpClient okHttpClinet = new OkHttpClient();
-    Response postJsonData(String httpUrl, String jsonData) throws IOException {
+    /*Response postJsonData(String httpUrl, String jsonData) throws IOException {
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
         RequestBody body = RequestBody.create(JSON, jsonData);
         String USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_1 like Mac OS X) " +
@@ -21,7 +21,7 @@ class GetWebContent {
                 .header("User-Agent", USER_AGENT)
                 .build();
         return okHttpClinet.newCall(request).execute();
-    }
+    }*/
 
     Response getData(String httpUrl) throws IOException {
         String USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_1 like Mac OS X) " +
